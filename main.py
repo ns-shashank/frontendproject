919b082a17c95ffe95c2a25fa1dfe632054c8654
ns-shashank <162679562+ns-shashank@users.noreply.github.com>
2024-10-03 16:37:57 +0000

    first commit

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
-@app.route('/shank')
-def index():
-    return 'I am Shashank'
+@app.route("/")
+def shashank():
+  return render_template('home.html')  # Use the correct template name here
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
