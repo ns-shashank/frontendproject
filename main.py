02847d86526b9ac596e4ea482fe6f3b6c4177afa
ns-shashank <162679562+ns-shashank@users.noreply.github.com>
2024-10-04 18:02:37 +0000

    Added dynamic data and api route

--- main.py
+++ main.py
@@ -1,11 +1,38 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
+
+JOBS = [{
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Bengaluru, India',
+    'salary': 'Rs. 10,00,000'
+}, {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Delhi, India',
+    'salary': 'Rs. 15,00,000'
+}, {
+    'id': 3,
+    'title': 'FrontEnd Engineer',
+    'location': 'Remote',
+    'salary': ''
+}, {
+    'id': 4,
+    'title': 'BackEnd Engineer',
+    'location': 'hydrabad, India',
+    'salary': 'Rs. 12,00,000'
+}]
 
 
 @app.route('/')
 def shashank():
-  return render_template("index.html")
+  return render_template("index.html", jobs=JOBS, Company_name="Shashank")
+
+
+@app.route('/api/jobs')
+def list_jobs():
+  return jsonify(JOBS)
 
 
 if __name__ == '__main__':
