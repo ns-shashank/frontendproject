097d4a547dff5c9d21ab18be00d3a58089f8745e
ns-shashank <162679562+ns-shashank@users.noreply.github.com>
2024-10-04 11:39:06 +0000

    Added html and css

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route('/')
 def shashank():
-  return render_template('home.html')  # Use the correct template name here
+  return render_template("index.html")
+
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
